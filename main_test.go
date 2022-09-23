3e64127fc4448d2a817a600be40d9ad5016b2dcf
albert.fort <albert.fort@aquilessolutions.com>
2022-09-23 12:02:35 +0200

    Add Unit Test

---
+++ main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Test_getAlbums(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			getAlbums(tt.args.c)
+		})
+	}
+}
+
+func Test_postAlbums(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			postAlbums(tt.args.c)
+		})
+	}
+}
+
+func Test_getAlbumByID(t *testing.T) {
+	type args struct {
+		c *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			getAlbumByID(tt.args.c)
+		})
+	}
+}
